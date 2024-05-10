e57baf58e785b0bdf23fea5eecf5e3853d0b60b3
u03013112 <u03013112@hotmail.com>
2024-05-10 11:59:00 +0800

    debug

--- src/lookKill.py
+++ src/lookKill.py
@@ -3,43 +3,65 @@ import os
 import pandas as pd
 
 class LookKill:
-    def __init__(self,qipuPath=os.getcwd()+'/../qipu'):
+    def __init__(self,qipuPath=os.getcwd()+'/../qipu',isRandom=True):
         self.qipuPath = qipuPath
         self.df = self.readAllCsv(qipuPath)
+        self.currentQuestionCount = 0
+        if isRandom:
+            self.df = self.df.sample(frac=1).reset_index(drop=True)
 
-    def readCsv(self,csvFilename):
+    def readCsv(self, csvFilename):
         df = pd.read_csv(csvFilename)
-            
-        ret = pd.DataFrame(columns=['filename','idx','color','fen','bestMoveFen1','step'])
 
-        for i in range(len(df)):
-            idx = df['idx'][i]
-            fen = df['fen'][i]
-            bestMoveFen1 = df['best_move1_fen'][i]
-            bestScore1 = df['best_score1'][i]
+        # 使用pandas过滤满足条件的行
+        filtered_df = df[df['best_score1'] > 9000]
+
+        ret = pd.DataFrame(columns=['filename', 'idx', 'color', 'fen', 'bestMoveFen1', 'step'])
+
+        for i in range(len(filtered_df)):
+            idx = filtered_df.iloc[i]['idx']
+            fen = filtered_df.iloc[i]['fen']
+            bestMoveFen1 = filtered_df.iloc[i]['best_move1_fen']
+            bestScore1 = filtered_df.iloc[i]['best_score1']
             color = 'red' if idx % 2 == 1 else 'black'
-            if bestScore1 > 9000:
-                step = (10000 - bestScore1) / 100
-                new_row = pd.DataFrame({
-                    'filename': [csvFilename],
-                    'idx': [idx],
-                    'color': [color],
-                    'fen': [fen],
-                    'bestMoveFen1': [bestMoveFen1],
-                    'step': [step]
-                })
-                ret = ret.append(new_row, ignore_index=True)
-            
+            step = (10000 - bestScore1) / 100
+            new_row = pd.DataFrame({
+                'filename': [csvFilename],
+                'idx': [idx],
+                'color': [color],
+                'fen': [fen],
+                'bestMoveFen1': [bestMoveFen1],
+                'step': [step]
+            })
+            ret = pd.concat([ret, new_row], ignore_index=True)
+
         return ret
+
     
     def readAllCsv(self, path):
-        ret = pd.DataFrame(columns=['filename', 'idx', 'color', 'fen', 'bestMoveFen1', 'step'])
+        dfs = []  # 创建一个空列表来存储每个CSV文件的DataFrame
         for filename in os.listdir(path):
             if filename.endswith('.csv'):
                 csv_path = os.path.join(path, filename)
                 df = self.readCsv(csv_path)
-                ret = ret.append(df, ignore_index=True)
+                dfs.append(df)  # 将DataFrame添加到列表中
+        ret = pd.concat(dfs, ignore_index=True)  # 使用pd.concat合并所有的DataFrame
         return ret
+
+    def getCurrentQuestion(self):
+        return self.df.iloc[self.currentQuestionCount]
+    
+    def nextQuestion(self):
+        self.currentQuestionCount += 1
+        if self.currentQuestionCount >= len(self.df):
+            self.currentQuestionCount = 0
+        return self.getCurrentQuestion()
+    
+    def prevQuestion(self):
+        self.currentQuestionCount -= 1
+        if self.currentQuestionCount < 0:
+            self.currentQuestionCount = len(self.df) - 1
+        return self.getCurrentQuestion()
 
 if __name__ == '__main__':
     lookKill = LookKill()
--- src/train.py
+++ src/train.py
@@ -9,6 +9,7 @@
 # 右侧添加一个棋盘等待展示棋谱
 
 import tkinter as tk
+from lookKill import LookKill
 from ChessBoard import ChessBoard
 
 class SimpleChessGui(tk.Tk):
@@ -17,14 +18,67 @@ class SimpleChessGui(tk.Tk):
         self.title('简单棋盘展示')
         self.geometry('500x420')
         self.initUI()
+        # mode 暂时只有init和lookKill两种
+        self.mode = 'init'
+        self.lookKill = None
 
     def initUI(self):
-        self.btnWatchKill = tk.Button(self, text='看杀', width=10)
+        self.btnWatchKill = tk.Button(self, text='看杀', width=10, command=self.watchKill)
         self.btnWatchKill.grid(row=0, column=0, pady=5)
+
+        self.btnPrev = tk.Button(self, text='上一题', command=self.prevQuestion)
+        self.btnPrev.grid(row=1, column=0, pady=5)
+
+        self.btnNext = tk.Button(self, text='下一题', command=self.nextQuestion)
+        self.btnNext.grid(row=2, column=0, pady=5)
+
+        self.textLabel = tk.Text(self, wrap=tk.WORD, height=4,width=25)
+        self.textLabel.grid(row=7, column=0)
+        self.textLabel.tag_configure('red', foreground='red')
+        self.textLabel.tag_configure('black', foreground='black')
+        self.textLabel.tag_configure('blue', foreground='blue')
+        self.textLabel.config(state=tk.DISABLED)
 
         self.chessBoard = ChessBoard(self, width=300, height=350)
         self.chessBoard.draw_board(style=2)
         self.chessBoard.grid(row=0, column=1, rowspan=8, padx=20)
+
+    def watchKill(self):
+        print('看杀')
+        if self.lookKill is None:
+            self.lookKill = LookKill()
+            self.mode = 'lookKill'
+
+    def updateText(self):
+        self.textLabel.config(state=tk.NORMAL)
+        self.textLabel.delete('1.0', tk.END)
+
+        if self.mode == 'lookKill':
+            question = self.lookKill.getCurrentQuestion()
+            color = question['color']
+            colorZ = '红' if color == 'red' else '黑'
+            step = question['step']
+            self.textLabel.insert(tk.END, f'{colorZ}方', color)
+            self.textLabel.insert(tk.END, f' {step}步杀\n', 'blue')
+        else:
+            self.textLabel.insert(tk.END, '请先选择模式')
+
+    def prevQuestion(self):
+        if self.mode == 'lookKill':
+            question = self.lookKill.prevQuestion()
+            self.chessBoard.readFen(question['fen'])
+            self.updateText()
+        else:
+            print('请先选择模式')
+
+    def nextQuestion(self):
+        if self.mode == 'lookKill':
+            question = self.lookKill.nextQuestion()
+            self.chessBoard.readFen(question['fen'])
+            self.updateText()
+        else:
+            print('请先选择模式')
+        
 
 if __name__ == '__main__':
     app = SimpleChessGui()
