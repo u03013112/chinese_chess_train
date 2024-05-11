904d05797b8774a91f273abdef3bdeeef35312f1
u03013112 <u03013112@hotmail.com>
2024-05-11 16:38:33 +0800

    1

--- src/export.py
+++ src/export.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import numpy as np
 import pandas as pd
 from img2Fen import Img2Fen
 from tools import getMove
@@ -68,34 +69,75 @@ def test(video_filename):
 
     # 读取第一帧
     ret, frame = cap.read()
-    # cv2.imshow("frame", frame)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # print('视频分辨率:', frame.shape[1], frame.shape[0])
 
     # 检测棋盘
     if exporter.detectBoard(frame):
         print("棋盘检测成功，开始导出棋谱。")
 
-        # 逐帧处理视频
-        while ret:
-            # 导出FEN
-            exporter.exportFen(frame)
+        lastFrame = None
+        lastFrameProcessed = False
 
-            # 读取下一帧
+        count = 0
+        while ret:
+            count += 1
+            if lastFrame is None:
+                lastFrame = frame.copy()
+                lastFrameProcessed = False
+            else:
+                # 计算两帧之间的差异
+                diff = cv2.absdiff(frame, lastFrame)
+                # 将差异转换为二值图像，以便更清楚地看到差异
+                _, diff = cv2.threshold(diff, 150, 255, cv2.THRESH_BINARY)
+                # 如果两帧之间的差异大于阈值，则更新lastFrame并将lastFrameProcessed设置为False
+                if np.any(diff):
+                    lastFrame = frame.copy()
+                    lastFrameProcessed = False
+                else:
+                    
+                    if not lastFrameProcessed:
+                        print(count)
+                        # exporter.exportFen(lastFrame)
+
+                        cv2.imshow('frame', lastFrame)
+                        cv2.waitKey(0)
+                        cv2.destroyAllWindows()
+                        
+                        lastFrameProcessed = True
+
             ret, frame = cap.read()
-            # cv2.imshow("frame", frame)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
     else:
         print("棋盘检测失败，请重试。")
 
-    # 保存结果
-    exporter.save()
+    # exporter.save()
+    cap.release()
 
-    # 释放视频资源
+def compare_first_two_frames(video_filename):
+    cap = cv2.VideoCapture(video_filename)
+
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+        return
+
+    # 读取第一帧和第二帧
+    ret, frame1 = cap.read()
+    ret, frame2 = cap.read()
+
+    # 遍历所有可能的阈值
+    for threshold in range(1, 256):
+        # 计算两帧之间的差异
+        diff = cv2.absdiff(frame1, frame2)
+
+        # 将差异转换为二值图像
+        _, diff = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
+
+        # 如果二值图像中没有白色像素（值为255），则认为两帧一致
+        is_diff = np.any(diff)
+
+        print(f"Threshold: {threshold}, is_diff: {is_diff}")
+
     cap.release()
 
 if __name__ == '__main__':
     video_filename = "../screenSnapshot/2024-05-11_12-24-13.avi"
     test(video_filename)
+    # compare_first_two_frames(video_filename)
